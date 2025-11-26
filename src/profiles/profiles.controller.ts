767a137a835807527021d25bf199c3b28dc24007
Reda <mahmoudreda457@gmail.com>
2025-11-27 00:04:18 +0200

    profiles GET & POST

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+
+@Controller('profiles')
+export class ProfilesController {
+  // GET /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{ location }];
+  }
+
+  // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+
+  // PUT /profiles/:id
+  // DELETE /profiles/:id
+}
