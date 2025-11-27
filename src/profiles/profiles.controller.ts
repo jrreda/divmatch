de98fe9aa5f17c36c23b34ca6f13becef358ec5e
Reda <mahmoudreda457@gmail.com>
2025-11-27 11:05:13 +0200

    prodile basic crud

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  HttpCode,
+  HttpStatus,
+  Delete,
+} from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+
+@Controller('profiles')
+export class ProfilesController {
+  // GET /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{ location }];
+  }
+
+  // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+
+  // PUT /profiles/:id
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
+    return {
+      id,
+      name: updateProfileDto.name,
+      description: updateProfileDto.description,
+    };
+  }
+
+  // DELETE /profiles/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return {
+      id,
+      message: 'Profile deleted successfully',
+    };
+  }
+}
