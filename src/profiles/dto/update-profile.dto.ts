5935763e1dc5d7013124cb13cef770c869b87e14
Reda <mahmoudreda457@gmail.com>
2025-11-27 16:32:16 +0200

    pipes (Validation)

--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, Length } from 'class-validator';
+
 export class UpdateProfileDto {
-  name: string;
-  description: string;
+  @IsString()
+  @Length(3, 100)
+  name?: string;
+
+  @IsString()
+  @Length(10, 1000)
+  description?: string;
 }
